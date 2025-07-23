06b10bb44751da2b501eacca8e9adf3571049f12
RafaelDellaValle <rafaeldesousa3245@gmail.com>
2025-07-23 05:36:58 -0300

    mudança no código

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,22 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const[tarefa, setTarefa] = useState("");  //cria uma "caixa" para guardar o texto
+
+
   return (
-  <html>
-    <head>
-      <meta />
-      <title>Rafael</title>
-    </head>
-    <body className="Container">
-      <div >
+      <div className="Container">     
+       <title>Rafael</title>
         <div className="ToDoApp">
           <h2>To do list<img className="ImagemList" src='../src/img/icone-to-do.png'/></h2>
           <div className='Row'>
-            <input type="text" id='input-box' placeholder='add your text'></input>
+            <input 
+            type="text" 
+            value={tarefa}
+            onChange={(d) => setTarefa(d.target.value)}
+            placeholder='add your text'>
+            </input>
             <button>add</button>
           </div>
           <ul id='lis-container'>
-            <li>task1</li>
+            <li>task1{tarefa}</li>
             <li>task2</li>
             <li>task3</li>
 
@@ -24,9 +28,6 @@ function App() {
         </div>
         
       </div>
-    </body>
-  </html>
-
   )
 }
 
--- src/page/script.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react" 
-
-
-
-    
-    const input = document.getElementById("input-box")
